28d1447718e977d37c6790951ac6a9c42374fcf3
Leonardo-Neves <leosn784512@gmail.com>
2024-10-08 17:12:37 -0300

    Save changes

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -13,50 +13,35 @@ image_path = r"C:\Users\leosn\Desktop\PIM\datasets\MICCAI_BraTS_2020_Data_Traini
 nii_file = nib.load(image_path)
 nii_data = nii_file.get_fdata()
 
-j = 0
-
 for i in range(nii_data.shape[2]):
     axial_slice = nii_data[:, :, i]
 
-    # print(np.max(axial_slice))
-    # print(np.min(axial_slice))
-
-    # axial_slice = (axial_slice / np.max(axial_slice)) * 255
-
     axial_slice_8 = cv2.normalize(axial_slice, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-
-    # # if np.mean(axial_slice) != 0:
-
     axial_slice_8 = cv2.resize(axial_slice_8, (640, 640))
-
-    # # cv2.imshow(f'image {i}', cv2.equalizeHist(axial_slice_resized))
 
     if i == 77:
 
+        # Selecting only the region of the brain
         mask = np.where(axial_slice_8 == 0, 0, 1)
         mask = cv2.normalize(mask, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-
-        # cv2.imshow(f'oliginal', axial_slice_8)
-        
         non_uniform_region = cv2.bitwise_and(axial_slice_8, axial_slice_8, mask=mask)
-
         pixels = non_uniform_region[mask == 255]
 
-        mean_value = np.mean(pixels)
-        min_value = np.min(pixels)
-        max_value = np.max(pixels)
-        unique, counts = np.unique(pixels, return_counts=True)
+        # mean_value = np.mean(pixels)
+        # min_value = np.min(pixels)
+        # max_value = np.max(pixels)
+        # unique, counts = np.unique(pixels, return_counts=True)
 
         # print("Mean pixel value:", mean_value)
         # print("Min pixel value:", min_value)
         # print("Max pixel value:", max_value)
         # print("Pixel value distribution (Intensity : Frequency):", dict(zip(unique, counts)))
 
-        plt.bar(unique, counts)
-        plt.show()
+        # plt.bar(unique, counts)
+        # plt.show()
         
+        # Histogram Equalization
         hist, bins = np.histogram(pixels, 256, [0, 256])
-
         cdf = hist.cumsum()
         cdf_normalized = cdf * float(hist.max()) / cdf.max()
         cdf_masked = np.ma.masked_equal(cdf, 0)
@@ -64,40 +49,31 @@ for i in range(nii_data.shape[2]):
         cdf_final = np.ma.filled(cdf_masked, 0).astype('uint8')
         image_equalized = cdf_final[axial_slice_8]
 
-        non_uniform_region = cv2.bitwise_and(image_equalized, image_equalized, mask=mask)
-        pixels = non_uniform_region[mask == 255]
-        unique, counts = np.unique(pixels, return_counts=True)
-        plt.bar(unique, counts)
-        plt.show()
+        # non_uniform_region = cv2.bitwise_and(image_equalized, image_equalized, mask=mask)
+        # pixels = non_uniform_region[mask == 255]
+        # unique, counts = np.unique(pixels, return_counts=True)
+        # plt.bar(unique, counts)
+        # plt.show()
+
+        # Filtering images to remove paper and salt noise
+        image = np.array(image_equalized)
+
+        filtered_image = np.zeros_like(image)
+
+        def medianFilter(matrix_neighborhood):
+            return np.median(matrix_neighborhood)
+
+        for i in range(1, image.shape[0]-1):
+            for j in range(1, image.shape[1]-1):
+                neighborhood = np.array([[image[i-1, j-1], image[i-1, j],   image[i-1, j+1]],
+                                        [image[i, j-1],   image[i-1, j-1], image[i, j+1]],
+                                        [image[i+1, j-1], image[i+1, j],   image[i+1, j+1]]])
+
+                filtered_image[i, j]= medianFilter(neighborhood)
 
         cv2.imshow(f'image_equalized', image_equalized)
+        cv2.imshow(f'filtered_image', filtered_image)
 
         plt.show()
         cv2.waitKey(0) 
 
-
-        # brain_region = cv2.bitwise_and(axial_slice, axial_slice, mask=mask)
-        
-        # non_zero_pixels = axial_slice[axial_slice > 0]
-
-        # brain_region_equalized = cv2.equalizeHist(brain_region)
-
-        # image_dataframe = dip.probabilityIntensity(non_zero_pixels)
-
-        
-    #     (h, w) = axial_slice_resized.shape[:2]
-
-    #     center = (w // 2, h // 2)
-
-    #     rotation_matrix = cv2.getRotationMatrix2D(center, -90, 1)
-
-    #     rotated_image = cv2.warpAffine(axial_slice_resized, rotation_matrix, (w, h))
-
-    #     rotated_image = cv2.equalizeHist(rotated_image)
-
-
-        
-
-       
-
-    j += 1
--- slice_nii_images.py
+++ slice_nii_images.py
@@ -4,60 +4,111 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 
-ROOT_PATH = 'datasets/MICCAI_BraTS_2019_Data_Training'
+ROOT_PATH = 'datasets/MICCAI_BraTS_2020_Data_Training/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData'
 
-OUTPUT_PATH = 'datasets/MICCAI_BraTS_2019_Data_Training/HGG_PNG'
+OUTPUT_PATH = 'datasets/MICCAI_BraTS_2020_Data_Training/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingDataSliced'
 
 os.makedirs(OUTPUT_PATH, exist_ok=True)
 
-def normalize_slice(slice_data):
-    slice_normalized = (slice_data - np.min(slice_data)) / (np.max(slice_data) - np.min(slice_data)) * 255
-    return slice_normalized.astype(np.uint8)
+def preProcessingPhase(image):
+    # Normalizing the image to 8 bits and rezising it
+    image_8bits = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    image_8bits = cv2.resize(image_8bits, (640, 640))
 
-for folder_name in os.listdir(os.path.join(ROOT_PATH, 'HGG')):
+    # Selecting only the region of the brain
+    mask = np.where(image_8bits == 0, 0, 1)
+    mask = cv2.normalize(mask, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    non_uniform_region = cv2.bitwise_and(image_8bits, image_8bits, mask=mask)
+    pixels = non_uniform_region[mask == 255]
+    
+    # Histogram Equalization
+    hist, bins = np.histogram(pixels, 256, [0, 256])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * float(hist.max()) / cdf.max()
+    cdf_masked = np.ma.masked_equal(cdf, 0)
+    cdf_masked = (cdf_masked - cdf_masked.min()) * 255 / (cdf_masked.max() - cdf_masked.min())
+    cdf_final = np.ma.filled(cdf_masked, 0).astype('uint8')
+    image_equalized = cdf_final[image_8bits]
 
-    nii_file = nib.load(os.path.join(ROOT_PATH, 'HGG', folder_name, f'{folder_name}_t1ce.nii'))
+    # Filtering images to remove paper and salt noise in spatial domain
+    image = np.array(image_equalized)
+
+    filtered_image_without_pepper_salt = np.zeros_like(image)
+
+    def medianFilter(matrix_neighborhood):
+        return np.median(matrix_neighborhood)
+
+    for i in range(1, image.shape[0]-1):
+        for j in range(1, image.shape[1]-1):
+            neighborhood = np.array([[image[i-1, j-1], image[i-1, j],   image[i-1, j+1]],
+                                    [image[i, j-1],   image[i-1, j-1], image[i, j+1]],
+                                    [image[i+1, j-1], image[i+1, j],   image[i+1, j+1]]])
+
+            filtered_image_without_pepper_salt[i, j]= medianFilter(neighborhood)
+
+    return filtered_image_without_pepper_salt
+
+def preProcessingPhaseSegmentationMask(image):
+    image_8bits = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    image_8bits = cv2.resize(image_8bits, (640, 640))
+
+    return np.where(image_8bits > 0, 255, 0)
+
+for folder_name in os.listdir(ROOT_PATH):
+
+    nii_file = nib.load(os.path.join(ROOT_PATH, folder_name, f'{folder_name}_t1ce.nii'))
     nii_data = nii_file.get_fdata()
+
+    nii_segmentation_file = nib.load(os.path.join(ROOT_PATH, folder_name, f'{folder_name}_seg.nii'))
+    nii_segmentation_data = nii_segmentation_file.get_fdata()
 
     os.makedirs(os.path.join(OUTPUT_PATH, folder_name), exist_ok=True)
     os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'axial'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'axial', 'slices'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'axial', 'slices_segmentation'), exist_ok=True)
+
     os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'coronal'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'coronal', 'slices'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'coronal', 'slices_segmentation'), exist_ok=True)
+
     os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'sagittal'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'sagittal', 'slices'), exist_ok=True)
+    os.makedirs(os.path.join(OUTPUT_PATH, folder_name, 'sagittal', 'slices_segmentation'), exist_ok=True)
 
     j = 0
 
     for i in range(nii_data.shape[2]):
         axial_slice = nii_data[:, :, i]
-        axial_slice_normalized = normalize_slice(axial_slice)
+        axial_slice_normalized = preProcessingPhase(axial_slice)
+        axial_slice_segmentation_normalized = preProcessingPhaseSegmentationMask(axial_slice)
 
-        axial_slice_normalized_resized = cv2.resize(axial_slice_normalized, (640, 640))
-
-        if np.mean(axial_slice_normalized_resized) != 0:
-            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'axial/axial_slice_{j}.png'), axial_slice_normalized_resized, cmap='gray')
+        if np.mean(axial_slice_segmentation_normalized) != 0:
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'axial/slices/axial_slice_{j}.png'), axial_slice_normalized, cmap='gray')
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'axial/slices_segmentation/axial_slice_{j}.png'), axial_slice_segmentation_normalized, cmap='gray')
             j += 1
 
     j = 0
 
     for i in range(nii_data.shape[1]):
         coronal_slice = nii_data[:, i, :]
-        coronal_slice_normalized = normalize_slice(coronal_slice)
+        coronal_slice_normalized = preProcessingPhase(coronal_slice)
+        coronal_slice_segmentation_normalized = preProcessingPhaseSegmentationMask(coronal_slice)
 
-        coronal_slice_normalized_resized = cv2.resize(coronal_slice_normalized, (640, 640))
-
-        if np.mean(coronal_slice_normalized_resized) != 0:
-            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'coronal/coronal_slice_{j}.png'), coronal_slice_normalized_resized, cmap='gray')
+        if np.mean(coronal_slice_normalized) != 0:
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'coronal/slices/coronal_slice_{j}.png'), coronal_slice_normalized, cmap='gray')
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'coronal/slices_segmentation/coronal_slice_{j}.png'), coronal_slice_segmentation_normalized, cmap='gray')
             j += 1
 
     j = 0
 
     for i in range(nii_data.shape[0]):
         sagittal_slice = nii_data[i, :, :]
-        sagittal_slice_normalized = normalize_slice(sagittal_slice)
+        sagittal_slice_normalized = preProcessingPhase(sagittal_slice)
+        sagittal_slice_segmentation_normalized = preProcessingPhaseSegmentationMask(sagittal_slice)
 
-        sagittal_slice_normalized_resized = cv2.resize(sagittal_slice_normalized, (640, 640))
-
-        if np.mean(sagittal_slice_normalized_resized) != 0:
-            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'sagittal/sagittal_slice_{j}.png'), sagittal_slice_normalized_resized, cmap='gray')
+        if np.mean(sagittal_slice_normalized) != 0:
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'sagittal/slices/sagittal_slice_{j}.png'), sagittal_slice_normalized, cmap='gray')
+            plt.imsave(os.path.join(OUTPUT_PATH, folder_name, f'sagittal/slices_segmentation/sagittal_slice_{j}.png'), sagittal_slice_segmentation_normalized, cmap='gray')
             j += 1
 
 print("All slices saved successfully!")
