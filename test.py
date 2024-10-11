0de965e96c2214a817309a956131de250f9eb5b4
Leonardo-Neves <leosn784512@gmail.com>
2024-10-11 18:21:37 -0300

    Save changes

--- test.py
+++ test.py
@@ -30,9 +30,12 @@ for i in range(nii_data.shape[2]):
         image_8bits = cv2.normalize(axial_slice, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
         image_8bits = cv2.resize(image_8bits, (640, 640))
 
+        image_8bits = cv2.GaussianBlur(image_8bits, (5, 5), 0)
+
         image_8bits_segmentation = cv2.normalize(axial_slice_segmentation, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
         image_8bits_segmentation = cv2.resize(image_8bits_segmentation, (640, 640))
 
+        # Slice
         mask_non_zero_region = np.where(image_8bits == 0, 0, 1)
         mask_non_zero_region = cv2.normalize(mask_non_zero_region, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
@@ -46,22 +49,35 @@ for i in range(nii_data.shape[2]):
 
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
+        # Slice Segmentation
+
+        image_8bits_segmentation = np.where(image_8bits_segmentation > 0, 255, 0)
+        image_8bits_segmentation = cv2.normalize(image_8bits_segmentation, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
         slices.append(mask)
+        slices_segmentation.append(image_8bits_segmentation)
 
-mask_mean = np.mean(slices, axis=0).astype(np.uint8)
 
-mask_non_zero_region = np.where(mask_mean == 0, 0, 1)
-mask_non_zero_region = cv2.normalize(mask_non_zero_region, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+def t(slices):
+    mask_mean = np.mean(slices, axis=0).astype(np.uint8)
 
-non_uniform_region = cv2.bitwise_and(mask_mean, mask_mean, mask=mask_non_zero_region)
-pixels = non_uniform_region[mask_non_zero_region == 255]
+    mask_non_zero_region = np.where(mask_mean == 0, 0, 1)
+    mask_non_zero_region = cv2.normalize(mask_non_zero_region, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
-threshold = round(dip.otsuThresholding(pixels))
+    non_uniform_region = cv2.bitwise_and(mask_mean, mask_mean, mask=mask_non_zero_region)
+    pixels = non_uniform_region[mask_non_zero_region == 255]
 
-mask_result = np.where(mask_mean >= threshold, 255, 0)
-mask_result = cv2.normalize(mask_result, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    threshold = round(dip.otsuThresholding(pixels))
 
-cv2.imshow('result', mask_result)
+    mask_result = np.where(mask_mean >= threshold, 255, 0)
+    # return cv2.normalize(mask_result, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    return mask_mean
+
+cv2.imshow('result', t(slices))
+cv2.imshow('seg', t(slices_segmentation))
+
+
+# cv2.imshow('dif', t(slices_segmentation) - t(slices))
 
 cv2.waitKey(0)  
 
