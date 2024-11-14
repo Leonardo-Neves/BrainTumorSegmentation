5014ab9dd080a4d7d849d1223cada20b507b08ce
Leonardo-Neves <leosn784512@gmail.com>
2024-11-13 21:33:25 -0300

    sAve changes

--- test.py
+++ test.py
@@ -1,18 +1,119 @@
 import pandas as pd
+import numpy as np
 import cv2
+import nibabel as nib
+import os
 
-image_path = r'C:\Users\leosn\Desktop\PIM\datasets\mask_mean\train\BraTS20_Training_001\sagittal.png'
+from utils.digital_image_processing import DigitalImageProcessing
+from utils.frequency_domain import FrequencyDomain
+from utils.spartial_domain import SpartialDomain
 
-image = cv2.imread(image_path)
+dip = DigitalImageProcessing()
+fd = FrequencyDomain()
+sd = SpartialDomain()
 
-dataframe = pd.read_csv('ellipses_indexes_mask_segmentation.csv', sep=';')
+def coronalSegmentation(image_path, initial_index, end_index):
 
-row = dataframe.iloc[0]
+    nii_file = nib.load(image_path)
+    nii_data = nii_file.get_fdata()
 
-ellipse_caracteristics = [(row['Sagittal_Center_X'], row['Sagittal_Center_Y']), (row['Sagittal_Width'], row['Sagittal_Height']), row['Sagittal_Angle']]
+    mask_mean_border = cv2.imread('images/mask_mean_coronal_border3.png', cv2.IMREAD_GRAYSCALE)
 
-cv2.ellipse(image, ellipse_caracteristics, 255, 2)
+    clahe = cv2.createCLAHE(clipLimit=2.1, tileGridSize=(12, 12))
 
-cv2.imshow('image', image)
+    processed_images = []
 
-cv2.waitKey(0)
+    cutoff_frequency = 40
+    c = 1
+
+    for i in range(nii_data.shape[1]):
+
+        if i >= initial_index and i <= end_index: # BraTS20_Training_003_t1ce.nii
+            axial_slice = nii_data[:, i, :]
+
+            image_8bits = cv2.normalize(axial_slice, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+            image_8bits = cv2.rotate(image_8bits, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            image_8bits = cv2.resize(image_8bits, (800, 640))
+
+            # Gaussian High-Pass Filter
+            padded_image = fd.padImage(image_8bits)
+            H_u_v = fd.gaussianHighpassFilter(cutoff_frequency, padded_image.shape)
+            mask_gaussian, F_u_v = fd.filterImage(image_8bits, padded_image, H_u_v)
+            image_8bits_filtered_gaussian = image_8bits + (c * mask_gaussian)
+            image_8bits_filtered_gaussian = cv2.normalize(image_8bits_filtered_gaussian, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
+            # Adaptive Histogram Equalization
+            image_8bits_ahe = clahe.apply(image_8bits_filtered_gaussian)
+
+            # cv2.imshow('image_8bits_ahe', image_8bits_ahe)
+
+            mask_non_zero_region = np.where(image_8bits_ahe > 0, 255, 0).astype(np.uint8)
+
+            mask = sd.leoThreshold(image_8bits_ahe, mask_non_zero_region, 19)
+
+            mask2 = sd.leoThreshold2(image_8bits_ahe, mask_non_zero_region, 19)
+
+            masks = []
+
+            # for i in [19, 21, 23, 25, 27, 29, 31, 33, 35, 37]:
+            # for i in [3, 7, 15, 19, 23, 29, 35, 41, 47, 53]:
+            # for i in [3, 5, 7, 9, 11, 13]:
+            #     masks.append(sd.leoThreshold(image_8bits_ahe, mask_non_zero_region, i))
+
+            # mask_mean_leo_threshold = np.mean(masks, axis=0).astype(np.uint8)
+
+            cv2.imshow('mask', mask)
+
+            cv2.imshow('mask2', mask2)
+
+            cv2.waitKey(0)
+
+            processed_images.append(mask)
+
+    mask_mean = np.mean(processed_images, axis=0).astype(np.uint8)
+
+    cv2.imshow('mask_mean', mask_mean)
+
+    cv2.waitKey(0)
+
+    return mask_mean
+
+
+
+ROOT_PATH = r'C:\Users\leosn\Desktop\PIM\datasets\MICCAI_BraTS_2020_Data_Training\BraTS2020_TrainingData\MICCAI_BraTS2020_TrainingData'
+
+dataframe_slices = pd.read_excel('slices.xlsx').dropna()
+
+for folder_name in os.listdir(ROOT_PATH):
+
+    image_path = os.path.join(ROOT_PATH, folder_name, f'{folder_name}_t1ce.nii')
+
+    # Selecting the row to the index of the image
+    image_index = int(folder_name.split('_')[-1])
+
+
+
+    dataframe_slices_filtered = dataframe_slices.loc[dataframe_slices['Index'] == image_index]
+
+    row = dataframe_slices_filtered.iloc[0]
+
+    if dataframe_slices_filtered.shape[0] == 0:
+        print(1)
+        break
+
+    # ------------------------------- Segmentation section -------------------------------
+
+    mask_mean_coronal = coronalSegmentation(image_path, row['Coronal_Initial'], row['Coronal_End'])
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils/spartial_domain.py
+++ utils/spartial_domain.py
@@ -25,30 +25,75 @@ class SpartialDomain:
                     global_mean = neighbor_mean
                     global_std = np.std(neighbor)
 
+        print('1- global_mean', global_mean)
+        print('1- global_std', global_std)
+
         return np.where((image >= (global_mean - global_std)) & (image <= (global_mean + global_std)), 255, 0).astype(np.uint8)
 
 
     def leoThreshold2(self, image, mask_image, kernel_size=3):
+        
+        mean = cv2.blur(image, (kernel_size, kernel_size))
 
+        squared_image = image ** 2
+        mean_of_squares = cv2.blur(squared_image, (kernel_size, kernel_size))
+
+        std_dev = np.sqrt((image - mean) ** 2)
+
+        i, j = np.unravel_index(np.argmax(mean), mean.shape)
+
+        global_mean = np.max(mean)
+        global_std = std_dev[i, j]
+
+        print('2- global_mean', global_mean)
+        print('2- global_std', global_std)
+
+        return np.where((image >= global_mean - global_std) & (image <= (global_mean + global_std)), 255, 0).astype(np.uint8)
+    
+    def leoThreshold3(self, image, mask_image, kernel_size=3):
+        """
+        Optimized version of leoThreshold that maintains the same behavior
+        but with improved performance.
+        """
+        # Initial ROI calculation
         region_of_interest = cv2.bitwise_and(image, image, mask=mask_image)
         roi_values = np.array(region_of_interest[region_of_interest > 0])
-
+        
+        # Initial statistics from ROI
         global_mean = np.mean(roi_values)
         global_std = np.std(roi_values)
-
+        
+        # Pre-calculate padding size
         sides = int((kernel_size - 1) / 2)
-
-        padded_image = np.pad(image, pad_width=sides, mode='constant', constant_values=0)
-
-        for i in range(1, image.shape[0]-1):
-            for j in range(1, image.shape[1]-1):
-                neighbor = np.array(padded_image[i-sides:i+(sides + 1), j-sides:j+(sides + 1)])
-                neighbor_mean = np.mean(neighbor)
-
-                if neighbor_mean > global_mean:
-                    global_mean = neighbor_mean
-                    global_std = np.std(neighbor)
-
-        return np.where((image >= global_mean) & (image <= (global_mean + global_std)), 255, 0).astype(np.uint8)
+        
+        # Use numpy's advanced indexing for faster neighborhood extraction
+        padded_image = np.pad(image, sides, mode='constant', constant_values=0)
+        
+        # Create views into the padded image for faster neighborhood access
+        windows = np.lib.stride_tricks.sliding_window_view(
+            padded_image, 
+            (kernel_size, kernel_size)
+        )[:-kernel_size+1, :-kernel_size+1]
+        
+        # Calculate means for all neighborhoods at once
+        neighbor_means = np.mean(windows, axis=(2, 3))
+        
+        # Find neighborhoods with means higher than global_mean
+        higher_means = neighbor_means > global_mean
+        
+        if np.any(higher_means):
+            # Update global statistics based on neighborhoods with higher means
+            max_neighbor_mean = np.max(neighbor_means[higher_means])
+            # Get the windows corresponding to the maximum mean
+            max_windows = windows[neighbor_means == max_neighbor_mean]
+            global_mean = max_neighbor_mean
+            global_std = np.std(max_windows)
+        
+        # Final thresholding
+        return np.where(
+            (image >= global_mean) & (image <= (global_mean + global_std)),
+            255, 
+            0
+        ).astype(np.uint8)
     
                     
